f3b8782518da140076dd2d107ad0844c2ae79576
SamMahan <SM1520@messiah.edu>
2018-07-05 11:06:52 -0400

    changes to testrun to attempt to fix errors

--- run/initBrowser.js
+++
@@ -1,7 +0,0 @@
-var webdriver = require("selenium-webdriver");
-var browser = new webdriver.Builder()
-  .usingServer()
-  .withCapabilities({ browserName: "chrome" })
-  .build();
-
-module.exports = { browser, webdriver };
